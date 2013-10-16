f36150dc740702bbd1db55dbdf112589e27fb976
Tim Radke <countach74@gmail.com>
2013-10-15 19:26:02 -0700

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,19 @@ from setuptools import setup, find_packages
 setup(
   name='gimme',
   version='0.1.0',
-  packages = [
+  packages=[
     'gimme',
     'gimme.adapters',
-    'gimme.ext'
+    'gimme.ext',
+    'gimme.servers',
+    'gimme.servers.http',
+    'gimme.servers.logger'
   ],
+  package_data={
+    'gimme': [
+      'templates/errors/*.html'
+    ]
+  },
   author='Tim Radke',
   author_email='countach74@gmail.com',
   license='MIT',
