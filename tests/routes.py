d1e16d10d98c9af54b3d9fc361728e97eab9b149
Tim Radke <countach74@gmail.com>
2014-06-26 17:17:30 -0700

    Moved back to unstable versioning; moving to stable (1.x.x) was a mistake. Also corrected some routes tests.

--- tests/routes.py
+++ tests/routes.py
@@ -7,40 +7,19 @@ from .test_helpers import make_environ
 class RoutesTest(unittest.TestCase):
   def setUp(self):
     self.app = gimme.App()
+    self.routes = self.app.routes
+    self.route = self.routes.get('/', None)
 
-    class TestController(gimme.Controller):
-        def endpoint1(self):
-            return 'endpoint1_response'
+  def test_get(self):
+    assert isinstance(self.route, gimme.routes.Route)
 
-    self.controller = TestController
+  def test_match(self):
+    assert self.routes.match({'REQUEST_METHOD': 'GET', 'PATH_INFO': '/'})
+    assert self.routes.match({'REQUEST_METHOD': 'POST', 'PATH_INFO': '/'})[1].status == 404
 
-  def test_query_string(self):
-    self.app.routes.get('/', self.controller.endpoint1) 
-
-    request, response, route = self.app.routes.match(make_environ(
-        uri='/?id=3'))
-
-    self.assertEqual(request.query.id, '3')
-
-  def test_param(self):
-    self.app.routes.get('/user/:id', self.controller.endpoint1)
-    
-    request, response, route = self.app.routes.match(make_environ(
-        uri='/user/4'))
-
-    self.assertEqual(request.params.id, '4')
-
-  def test_optional_param(self):
-    self.app.routes.get('/user/:id/:message?', self.controller.endpoint1)
-
-    request, response, route = self.app.routes.match(make_environ(
-        uri='/user/3/4'))
-
-    request2, response2, route2 = self.app.routes.match(make_environ(
-        uri='/user/3'))
-
-    self.assertEqual(request.params.id, '3')
-    self.assertTrue(bool(request2))
+  def test_get_route_by_name(self):
+    self.route.name('root')
+    assert self.routes.get_route_by_name('root') == self.route
 
 
 class RouteTest(unittest.TestCase):
